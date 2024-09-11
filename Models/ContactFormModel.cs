9468d107215a6c03b01017191778be17b00b7a09
kamran-Techsurge <kamran.iftikhar@techsurgeinc.com>
2024-09-11 10:30:06 +0500

    Updates to footer and layout file

---
+++ Models/ContactFormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Personal_Portfolio_Website.Models
+{
+    public class ContactFormModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
